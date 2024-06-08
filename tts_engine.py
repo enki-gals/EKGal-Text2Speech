c0ad41e43d64e06eebb46f5a5edf187421d8f9b0
hongsam14 <ghdtjdgus14@gmail.com>
2024-06-08 20:26:52 +0900

    Dev: develop text_to_audio func that returns wav stream

--- tts_engine.py
+++ tts_engine.py
@@ -5,56 +5,54 @@ from models import SynthesizerTrn
 import utils 
 import commons
 import sys
+import io
 import re
 import yaml
 from torch import no_grad, LongTensor
 import logging
 
-import simpleaudio
-
 logging.getLogger('numba').setLevel(logging.WARNING)
 
 class TTS_Engine():
     def __init__(self, yaml_path: str):
-        try:
-            __data: dict = self.__open_yaml(yaml_path)
-            # replace existing input course to yaml file
-            self.vits_model_path = __data['VITSModelPath']
-            print(f"model:{self.vits_model_path}")
-            self.config_file_path = __data['configFilePath']
-            print(f"config:{self.config_file_path}")
-            self.speaker_id = __data['speakerID']
-            print(f"speakerid: {self.speaker_id}")
-            self.hparams_ms = utils.get_hparams_from_file(self.config_file_path)
-
-            n_speakers = self.hparams_ms.data.n_speakers if 'n_speakers' in self.hparams_ms.data.keys() else 0
-            n_symbols = len(self.hparams_ms.symbols) if 'symbols' in self.hparams_ms.keys() else 0
-            print(f"n_symbols: {n_symbols}")
-            speakers = self.hparams_ms.speakers if 'speakers' in self.hparams_ms.keys() else ['0']
-            print(f"speakers: {speakers}")
-            use_f0 = self.hparams_ms.data.use_f0 if 'use_f0' in self.hparams_ms.data.keys() else False
-            print(f"use_f0: {use_f0}")
-            emotion_embedding = self.hparams_ms.data.emotion_embedding if 'emotion_embedding' in self.hparams_ms.data.keys() else False
-
-            self.net_g_ms = SynthesizerTrn(
-                n_symbols,
-                self.hparams_ms.data.filter_length // 2 + 1,
-                self.hparams_ms.train.segment_size,
-                n_speakers=n_speakers,
-                emotion_embedding=emotion_embedding,
-                **self.hparams_ms.model)
-            _ = self.net_g_ms.eval() 
-            utils.load_checkpoint(self.vits_model_path, self.net_g_ms)
-        except Exception as e:
-            print(e)
-            sys.exit(1)
-
+        __data: dict = self.__open_yaml(yaml_path)
+        # replace existing input course to yaml file
+        self.vits_model_path = __data['VITSModelPath']
+        print(f"model:{self.vits_model_path}")
+        self.config_file_path = __data['configFilePath']
+        print(f"config:{self.config_file_path}")
+        self.speaker_id = __data['speakerID']
+        print(f"speakerid: {self.speaker_id}")
+        self.hparams_ms = utils.get_hparams_from_file(self.config_file_path)
         
+        # init Moegoe sequence
+        self.__init_moegoe() 
+        _ = self.net_g_ms.eval() 
+        utils.load_checkpoint(self.vits_model_path, self.net_g_ms)
+
 
     def __open_yaml(self, yaml_path: str) -> dict:
         with open(yaml_path, 'r', encoding='utf-8') as file:
             data = yaml.safe_load(file)
         return data
+    
+    def __init_moegoe(self):
+        n_speakers = self.hparams_ms.data.n_speakers if 'n_speakers' in self.hparams_ms.data.keys() else 0
+        n_symbols = len(self.hparams_ms.symbols) if 'symbols' in self.hparams_ms.keys() else 0
+        print(f"n_symbols: {n_symbols}")
+        speakers = self.hparams_ms.speakers if 'speakers' in self.hparams_ms.keys() else ['0']
+        print(f"speakers: {speakers}")
+        use_f0 = self.hparams_ms.data.use_f0 if 'use_f0' in self.hparams_ms.data.keys() else False
+        print(f"use_f0: {use_f0}")
+        emotion_embedding = self.hparams_ms.data.emotion_embedding if 'emotion_embedding' in self.hparams_ms.data.keys() else False
+        #init object
+        self.net_g_ms = SynthesizerTrn(
+            n_symbols,
+            self.hparams_ms.data.filter_length // 2 + 1,
+            self.hparams_ms.train.segment_size,
+            n_speakers=n_speakers,
+            emotion_embedding=emotion_embedding,
+            **self.hparams_ms.model)
     
     def __get_label_value(self, text:str, label:str, default, warning_name:str='value'):
         value = re.search(rf'\[{label}=(.+?)\]', text)
@@ -84,32 +82,38 @@ class TTS_Engine():
             text_normalized = commons.intersperse(text_normalized, 0)
         text_normalized = LongTensor(text_normalized)
         return text_normalized
+
     
     def text_to_audio(self, text: str):
         length_scale, text = self.__get_label_value(text, 'LENGTH', 1, 'length scale')
-        print(f"length_scale: {length_scale}")
+        # print(f"length_scale: {length_scale}")
         noise_scale, text = self.__get_label_value(text, 'NOISE', 0.667, 'noise scale')
-        print(f"noise_scale: {noise_scale}")
+        # print(f"noise_scale: {noise_scale}")
         noise_scale_w, text = self.__get_label_value(text, 'NOISEW', 0.8, 'deviation of noise')
-        print(f"noise_scale_w: {noise_scale_w}")
+        # print(f"noise_scale_w: {noise_scale_w}")
         cleaned, text = self.__get_label(text, 'CLEANED')
-        print(f"cleaned: {cleaned}")
+        # print(f"cleaned: {cleaned}")
 
         sequence_text_normalized = self.__get_text(text, self.hparams_ms, cleaned=cleaned)
-        print(f"sequence_text_normalized: {sequence_text_normalized}")  
+        # print(f"sequence_text_normalized: {sequence_text_normalized}")  
         with no_grad():
             x_tst = sequence_text_normalized.unsqueeze(0)
             x_tst_lengths = LongTensor([sequence_text_normalized.size(0)])
             sid = LongTensor([self.speaker_id])
             audio = self.net_g_ms.infer(x_tst, x_tst_lengths, sid=sid, noise_scale=noise_scale,
                                         noise_scale_w=noise_scale_w, length_scale=length_scale)[0][0, 0].data.cpu().float().numpy()
-        write('audio.wav', self.hparams_ms.data.sampling_rate, audio)
+        bytes_wav = bytes()
+        bytes_io = io.BytesIO(bytes_wav)
+        write(bytes_io, self.hparams_ms.data.sampling_rate, audio)
+        return bytes_io.read()
         
 
 if __name__ == '__main__':
     try:
         tts_engine = TTS_Engine('config.yaml')
-        tts_engine.text_to_audio("こんにちは")
+        stream = tts_engine.text_to_audio('私は今日の状況を見なければなりません。')
+        with open('output.wav', 'wb') as f:
+            f.write(stream)
     except Exception as e:
         print(e)
         sys.exit(1)
